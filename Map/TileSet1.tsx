a4410a0f34d094382099656fa39c6f491090bfa3
Matilde Lucchesi <matilde.lucchesi3@stud.unifi.it>
2022-02-06 16:54:44 +0100

    Delete useless things

--- Map/TileSet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="TileSet1" tilewidth="60" tileheight="60" tilecount="112" columns="14">
- <image source="../images/mapTile/tileSet1.png" trans="ff00ff" width="848" height="530"/>
-</tileset>
